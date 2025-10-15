c848757a8b172e2d3114afe110a3a048a14724bf
Farah Saleem <fs8578058@gmail.com>
2025-10-15 04:19:23 -0700

    add slider in hero section

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let manuItem = document.querySelectorAll(".manuItem");
 
 console.log(manuItem);
 
-manuItem.forEach((item) => {
-
+manuItem.forEach((item, idx) => {
+item.addEventListener("click", () => {
+    console.log("item was clicked");
+    wrapperSlider.style.transform = `translateX(${-100 * idx}vw)`;
+})
 })
