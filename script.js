9c5eae748ee8c51c2d4128650503015ff765f992
Farah Saleem <fs8578058@gmail.com>
2025-10-15 03:55:47 -0700

    add hero section

---
+++ script.js
@@ -0,0 +1,9 @@
+let wrapperSlider = document.querySelector(".sliderwrapper");
+// let sliderItem = document.querySelectorAll(".sliderItem");
+let manuItem = document.querySelectorAll(".manuItem");
+
+console.log(manuItem);
+
+manuItem.forEach((item) => {
+
+})
