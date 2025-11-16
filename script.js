51be0343462c2346fcfda266aade805654985516
Farah Saleem <fs8578058@gmail.com>
2025-11-16 01:00:56 -0800

    add new update

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ let wrapperSlider = document.querySelector(".sliderwrapper");
 // let sliderItem = document.querySelectorAll(".sliderItem");
 let manuItem = document.querySelectorAll(".manuItem");
 
-console.log(manuItem);
+const products = [
+    {
+        id : 1,
+        title: "Air Force",
+        price: 119,
+        colors: [
+            {
+                code: "black",
+                img: "./img"
+            }
+        ]
+    }
+]
 
 manuItem.forEach((item, idx) => {
 item.addEventListener("click", () => {
